0f39423d14cfea64613b21229113c132b35e3fa5
Shahin <shahinu886@gmail.com>
2025-12-27 12:00:39 +0530

    update

--- router/src/App.jsx
+++ router/src/App.jsx
@@ -1,8 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
-function App() {
-  
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/register" />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
